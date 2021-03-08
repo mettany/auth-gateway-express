205a24a01f52313194868ac48f7d49acd9ed0497
Yoan Mettan <y.mettan@gmail.com>
2021-03-08 07:48:43 +0100

    ft - not exposing auth endpoint publicly

--- auth-gateway-express/index.js
+++ auth-gateway-express/index.js
@@ -6,12 +6,19 @@ const config = require("./lib/config");
 const { login, token, getUserInfos, authProxy } = require("./lib/auth");
 
 const app = express();
-app.use(session(config.session));
-app.use(cors({ origin: true, credentials: true}));
+const appAdmin = express();
+
+const sessionMiddlware = session(config.session);
+const corsMiddleware = cors({ origin: true, credentials: true});
+app.use(sessionMiddlware);
+appAdmin.use(sessionMiddlware);
+app.use(corsMiddleware);
+appAdmin.use(corsMiddleware);
 
 app.get("/login", login);
 app.get("/callback", token);
 app.get("/userinfos", getUserInfos);
-app.get("/auth", authProxy);
+appAdmin.get("/auth", authProxy);
 
 app.listen(config.port, () => console.log(`App listening on port ${config.port}`));
+appAdmin.listen(3001, () => console.log("AppAdmin listening on port 3001"));
